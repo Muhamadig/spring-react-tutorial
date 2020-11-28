5d3063dda0dfbc039b61bbfa7646dde549ae5903
MuhamadI <mohamadi@radware.com>
2020-11-28 18:55:07 +0200

    remove all

--- basic/src/main/java/com/github/muhamadig/reactandspringdatarestbasic/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.muhamadig.reactandspringdatarestbasic.payroll;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Long> {
-
-}
