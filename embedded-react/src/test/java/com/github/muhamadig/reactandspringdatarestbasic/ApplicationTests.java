c6f408ad0d474fad5317bf7ed0f848273bee0ac2
MuhamadI <mohamadi@radware.com>
2020-11-28 19:04:31 +0200

    init embedded

---
+++ embedded-react/src/test/java/com/github/muhamadig/reactandspringdatarestbasic/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.muhamadig.reactandspringdatarestbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
