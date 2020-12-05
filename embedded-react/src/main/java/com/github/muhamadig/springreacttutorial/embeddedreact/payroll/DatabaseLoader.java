5c970a112ee99bc955fb20d81d2daddb4356f2b0
MuhamadI <mohamadi@radware.com>
2020-12-05 18:11:44 +0200

    Add Paginations

--- embedded-react/src/main/java/com/github/muhamadig/springreacttutorial/embeddedreact/payroll/DatabaseLoader.java
+++ embedded-react/src/main/java/com/github/muhamadig/springreacttutorial/embeddedreact/payroll/DatabaseLoader.java
@@ -9,16 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-	private final EmployeeRepository repository;
+    private final EmployeeRepository repository;
 
     @Autowired
-	public DatabaseLoader(EmployeeRepository repository) {
-		this.repository = repository;
-	}
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
 
 
-	@Override
+
+
+    @Override
     public void run(String... strings) throws Exception {
         this.repository.save(new Employee("Muhamad", "Igbaria", "Infrastructure Full Stack Engineer"));
+        this.repository.save(new Employee("Eyal", "Itzaki", "Automation TL"));
+        this.repository.save(new Employee("Leanne ", "Graham", "Multi-layered client-server neural-net"));
+        this.repository.save(new Employee("Ervin ", "Howell", "Face to face bifurcated interface"));
+        this.repository.save(new Employee("Patricia ", "Dietrich", "User-centric fault-tolerant solution"));
+        this.repository.save(new Employee("Dennis  ", "Weissnat", "Configurable multimedia task-force"));
+        this.repository.save(new Employee("Nicholas   ", "Reichert", "Centralized empowering task-force"));
     }
 }
--- embedded-react/src/main/java/com/github/muhamadig/springreacttutorial/embeddedreact/payroll/EmployeeRepository.java
+++ embedded-react/src/main/java/com/github/muhamadig/springreacttutorial/embeddedreact/payroll/EmployeeRepository.java
@@ -1,8 +1,8 @@
 
 package com.github.muhamadig.springreacttutorial.embeddedreact.payroll;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
